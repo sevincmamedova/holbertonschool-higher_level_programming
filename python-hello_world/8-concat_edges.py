496e4979f97ea20ccba1b06b48c4643465931f4c
Sevish <sevish@Sevishs-MacBook-Air.local>
2026-06-16 13:20:46 +0400

    Fix 8-concat_edges indices

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -2,4 +2,4 @@
 str1 = "Python is an interpreted, interactive, object-oriented programming language"
 str2 = "Sarcasm is a lost art"
 str3 = "The Zen of Python, by Tim Peters: Beautiful is better than ugly"
-print(str1[28:] + " with " + str3[-6:])
+print(str1[39:] + " with " + str3[11:17])
