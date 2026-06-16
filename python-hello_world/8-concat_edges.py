ea866d05a34e1463b39760c52940d0067f662632
Sevish <sevish@Sevishs-MacBook-Air.local>
2026-06-16 14:24:33 +0400

    Fix 8-concat_edges

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-str1 = "Python is an interpreted, interactive, object-oriented programming language"
-str2 = "Sarcasm is a lost art"
-str3 = "The Zen of Python, by Tim Peters: Beautiful is better than ugly"
-print(str1[39:] + " with " + str3[11:17])
+str1 = "Python is an object-oriented programming language"
+str2 = "Sarcasm with a lost art"
+str3 = "The Zen of Python, by Tim Peters: Great is better than ugly"
+print(str1[13:] + str2[7:13] + str3[11:17])
