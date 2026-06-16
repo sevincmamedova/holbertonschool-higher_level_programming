1eb96fd142ca23f93301ddaa683559fc53df4543
Sevish <sevish@Sevishs-MacBook-Air.local>
2026-06-16 13:32:02 +0400

    Fix if in strings

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Python is an object-oriented programming language"
 str2 = "Sarcasm is a lost art"
-str3 = "The Zen of Python, by Tim Peters: Beautiful is better than ugly"
+str3 = "The Zen of Python, by Tim Peters: Great is better than ugly"
 print(str1[10:] + " with " + str3[11:17])
