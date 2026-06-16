dff858d21492a73289a3acfc073f62a6795689eb
Sevish <sevish@Sevishs-MacBook-Air.local>
2026-06-16 13:51:07 +0400

    Fix 8-concat_edges no string literals

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str1 = "Python is an object-oriented programming language"
-str2 = "Sarcasm is a lost art"
+str2 = "Sarcasm with a lost art"
 str3 = "The Zen of Python, by Tim Peters: Great is better than ugly"
-print(str1[13:] + " with " + str3[11:17])
+print(str1[13:] + str2[7:13] + str3[11:17])
