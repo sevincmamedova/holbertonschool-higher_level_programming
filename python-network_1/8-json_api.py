89aee27e509f584f2c4c45f10ab09d420dc2eeda
Sevinc <senin@email.com>
2026-06-27 01:06:34 +0400

    Add 8-json_api.py

---
+++ python-network_1/8-json_api.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Sends a POST request and displays JSON result"""
+import requests
+import sys
+
+if __name__ == "__main__":
+    q = sys.argv[1] if len(sys.argv) > 1 else ""
+    response = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+    try:
+        data = response.json()
+        if data:
+            print("[{}] {}".format(data.get('id'), data.get('name')))
+        else:
+            print("No result")
+    except Exception:
+        print("Not a valid JSON")
