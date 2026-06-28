b4628849ac59936b805859ed0f1e263b52abd331
Sevinc <sevishmamedova0@gmail.com>
2026-06-28 11:58:09 +0400

    Fix 8-concat_edges.py

--- python-hello_world/8-concat_edges.py
+++ python-hello_world/8-concat_edges.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programming\
  language that combines remarkable power with very clear syntax"
-print(str[0:][:11] + str[51:] [:38] + str[:10])
+str = str[0:11] + str[51:89] + str[0:6]
+print(str)
